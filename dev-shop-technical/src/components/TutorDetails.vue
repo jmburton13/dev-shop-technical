26dc8f196945d664cbc155fcbfa19198cc2e7dbb
jmburton13 <jmburton13@gmail.com>
2021-04-23 20:54:41 -0400

    content added to overlay--styling still needed

--- dev-shop-technical/src/components/TutorDetails.vue
+++ dev-shop-technical/src/components/TutorDetails.vue
@@ -4,11 +4,25 @@
             <img src="../assets/eduIcon.svg" />
             <h5 class="mt-2">{{ tutor.name }}</h5>
         </div>
-        <div>
+        <div class="d-flex flex-column align-items-center">
            <div class="d-flex verified-icon justify-content-center align-items-center">
                 <img v-if="tutor.verified" src="../assets/verifiedIcon.svg" />
            </div>
             <img id="tutor-image" class="card-img-top" :src="tutor.tutorImage" />
+       </div>
+       <div class="d-flex flex-column align-items-center">
+           <h5>{{ tutor.grades }}</h5>
+           <h5>{{ tutor.subjects }}</h5>
+           <h4>About {{ tutor.name }}</h4>
+            <p>{{ tutor.about }}</p>
+            <h4>Resume/Work Experience</h4>
+            <ul>
+                <li v-for="(item, index) in tutor.resume" :key="index"><span>{{ item }}</span></li>
+            </ul>
+            <h4>Certifications</h4>
+            <ul>
+                <li v-for="(item, index) in tutor.certifications" :key="index"><span>{{ item }}</span></li>
+            </ul>
        </div>
     </div>
 </template>
@@ -39,13 +53,30 @@ h5 {
 }
 
 .verified-icon {
-    position: absolute;
-    top: 88px;
-    left: 0px;
-    height: 40px;
-    width: 40px;
+    position: relative;
+    top: 160px;
+    left: -55px;
+    height: 50px;
+    width: 50px;
     background: #FFFFFFCC;
     border-top-right-radius: 20px;
+}
+
+p {
+    color: #707070;
+}
+
+ul {
+    list-style-position: inside;
+}
+
+li {
+    font-size: 13px;
+}
+
+span {
+    position: relative;
+    left: -10px;
 }
 
 </style>
